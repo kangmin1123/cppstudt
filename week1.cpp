7c74b1b96da5fa2c048b381db05ba4ffb4fda729
kangmin1123 <68904377+kangmin1123@users.noreply.github.com>
2025-02-25 21:51:17 +0900

    Add files via upload

---
+++ week1.cpp
@@ -0,0 +1,49 @@
+//1.1
+/*
+int main(){
+    return 0;
+}
+*/
+
+//1.2
+/*
+int main(){
+    return -1;
+}
+*/
+
+//1.3
+/*
+#include <iostream>
+
+int main(){
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+*/
+
+//1.4
+/*
+#include <iostream>
+int main() {
+    std::cout << "Enter two numbers:" << std::endl;
+    int x, y;
+    std::cin >> x >> y;
+    std::cout << "The product of " << x << " and " << y << " is " << x * y << std::endl;
+    return 0;
+}
+*/
+
+//1.5
+/*
+#include <iostream>
+int main() {
+    std::cout << "Enter two numbers:" << std::endl;
+    int x = 0, y = 0;
+    std::cin >> x >> y;
+    std::cout << "The sum of " << x ;
+    std::cout << " and " << y << " is ";
+    std::cout << x + y << std::endl;
+    return 0;
+}
+*/
